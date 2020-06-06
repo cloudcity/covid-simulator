3869e4c6f7f31a2b5813691040a51bc140a00f83
Andre Arko <andre@arko.net>
2020-06-05 21:54:59 -0700

    add compartment tabs, styles, use CovidGraph

--- src/CovidSimulator.svelte
+++ src/CovidSimulator.svelte
@@ -1,11 +1,65 @@
 <script>
   export let covidGraph
   export let region
-  import CovidSimulator from "./covid-simulator"
 
-  const capitalize = ws => ws.split(' ').map(w => w[0].toUpperCase()+ w.substring(1)).join(' ')
+  import _ from "lodash"
+  import CovidGraph from "./CovidGraph.svelte"
+
+  $: region = _.capitalize(region)
+  let simulatorType = covidGraph
+
+  let compartment = "Infected"
+  let interventions = {
+    "Shelter in place": [30, 160],
+  }
 </script>
 
-<div class="covid-simulator">
-  <h1>{capitalize(region)}</h1>
-</div>
+<style>
+  h1 {
+    margin-bottom: 0;
+    font-size: 24pt;
+  }
+
+  .subtitle {
+    margin-top: -0.5em;
+  }
+
+  .compartment-tabs {
+    padding-left: 2rem;
+  }
+
+  .compartment-tabs button {
+    background-color: transparent;
+    border: none;
+    line-height: 1.75rem;
+    font-size: 10pt;
+    width: 8rem;
+    color: #D35C08;
+  }
+
+  .compartment-tabs button.active {
+    color: #4A495B;
+    border-top: 2px solid #4A495B;
+    font-weight: bold;
+    background-color: #F7F7F8;
+  }
+
+  button:focus {outline:0;}
+</style>
+
+<section class="covid-simulator">
+  <section class="compartments">
+    <h1>{region}</h1>
+    <p class="subtitle">per million population</p>
+
+    <nav class="compartment-tabs">
+      <button class:active="{compartment === 'Infected'}" on:click="{() => compartment = 'Infected'}">Infections</button>
+      <button class:active="{compartment === 'Dead'}" on:click="{() => compartment = 'Dead'}">Deaths</button>
+    </nav>
+    <CovidGraph region={region} interventions={interventions} compartment={compartment} />
+  </section>
+
+  <section class="controls">
+    <p>slider here</p>
+  </section>
+</section>
