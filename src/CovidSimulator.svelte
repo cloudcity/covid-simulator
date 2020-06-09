35bd2fbb3de250edadc3e2edf389056d88356b7e
Andre Arko <andre@arko.net>
2020-06-08 22:14:01 -0700

    add stacked controls, customize radio button styles

--- src/CovidSimulator.svelte
+++ src/CovidSimulator.svelte
@@ -7,7 +7,8 @@
   import CovidGraph from "./CovidGraph"
 
   $: region = region[0].toUpperCase() + region.slice(1)
-  let simulatorType = covidGraph
+  let stacked = (covidGraph === "stacked")
+  let simple = (covidGraph === "simple")
 
   let compartment = "Infected"
   let interventions = {
@@ -54,9 +55,15 @@
   button:focus {outline:0;}
 
   .controls {
-    background-color: #F7EBDE;
     font-size: 15px;
+  }
+
+  .control {
     padding: 1.75rem 2.5rem;
+  }
+
+  .control-highlight {
+    background-color: #F7EBDE;
   }
 
   .control-group {
@@ -67,22 +74,85 @@
     font-weight: bold;
   }
 
-  .control-label {
+  .simple .control-label {
     display: inline-block;
     margin-top: 25px;
     width: 10%;
     text-align: center;
   }
 
-  .slider-container {
+  .simple .slider-container {
     position: absolute;
     top: 33px;
     right: 0;
     width: 90%;
   }
+
+  .stacked {
+    display: flex;
+    outline: 1px solid red;
+  }
+
+  .stacked .compartments {
+    flex: 70%;
+  }
+
+  .stacked .controls {
+    flex: 30%;
+    order: -1;
+    display: flex;
+    flex-flow: column nowrap;
+  }
+
+  .stacked .control {
+    padding: 1rem 0;
+  }
+
+  .stacked .control-label {
+    width: 100%;
+  }
+
+  .radio-container {
+    display: inline-block;
+    position: relative;
+    padding: 0 6px;
+    margin: 10px 0 0;
+  }
+
+  .radio-container input[type='radio'] {
+    display: none;
+  }
+
+  .radio-container label {
+  }
+
+  .radio-container label:before {
+    content: " ";
+    display: inline-block;
+    position: relative;
+    top: 2px;
+    margin: 0 3px 0 0;
+    width: 12px;
+    height: 12px;
+    border-radius: 10px;
+    border: 1px solid #D5D5DE;
+    background-color: transparent;
+  }
+
+  .radio-container input[type=radio]:checked + label:after {
+    border-radius: 11px;
+    width: 8px;
+    height: 8px;
+    position: absolute;
+    top: 5px;
+    left: 9px;
+    content: " ";
+    display: block;
+    background: #D35C08;
+  }
 </style>
 
-<section class="covid-simulator">
+<section class="covid-simulator" class:stacked class:simple>
   <section class="compartments">
     <h1 class="title">{region}</h1>
     <p class="subtitle">per million population</p>
@@ -95,7 +165,28 @@
   </section>
 
   <section class="controls">
+    {#if stacked}
     <section class="control">
+      <div class="control-name">Contact quarantine strategy</div>
+      <div class="control-group">
+        <div class="radio-container">
+          <input type="radio" id="strategy-quarantine" name="strategy" value="quarantine" checked>
+          <label class="control-label" for="strategy-quarantine">Household quarantine</label>
+        </div>
+        <div class="radio-container">
+          <input type="radio" id="strategy-tracing" name="strategy" value="tracing">
+          <label class="control-label" for="strategy-tracing">Extended contact tracing</label>
+        </div>
+      </div>
+      <div class="control-group">
+        <span class="control-label">Day</span>
+        <div class="slider-container">
+          <RangeSlider max="{300}" step="{1}" tooltip="{tooltip}" bind:values="{interventions["Shelter in place"]}"></RangeSlider>
+        </div>
+      </div>
+    </section>
+    {/if}
+    <section class="control control-highlight">
       <div class="control-name">Shelter in place</div>
       <div class="control-group">
         <span class="control-label">Day</span>
@@ -104,5 +195,34 @@
         </div>
       </div>
     </section>
+    {#if stacked}
+    <section class="control">
+      <div class="control-name">Schools closed</div>
+      <div class="control-group">
+        <span class="control-label">Day</span>
+        <div class="slider-container">
+          <RangeSlider max="{300}" step="{1}" tooltip="{tooltip}" bind:values="{interventions["Shelter in place"]}"></RangeSlider>
+        </div>
+      </div>
+    </section>
+    <section class="control">
+      <div class="control-name">Mass gatherings cancelled</div>
+      <div class="control-group">
+        <span class="control-label">Day</span>
+        <div class="slider-container">
+          <RangeSlider max="{300}" step="{1}" tooltip="{tooltip}" bind:values="{interventions["Shelter in place"]}"></RangeSlider>
+        </div>
+      </div>
+    </section>
+    <section class="control">
+      <div class="control-name">Shielding the elderly</div>
+      <div class="control-group">
+        <span class="control-label">Day</span>
+        <div class="slider-container">
+          <RangeSlider max="{300}" step="{1}" tooltip="{tooltip}" bind:values="{interventions["Shelter in place"]}"></RangeSlider>
+        </div>
+      </div>
+    </section>
+    {/if}      
   </section>
 </section>
