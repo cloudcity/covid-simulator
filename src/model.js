8024b12318c815706f171d44556a17a2cec443b7
Andre Arko <andre@arko.net>
2020-06-12 21:20:11 -0700

    set the total numbers back to the total

--- src/model.js
+++ src/model.js
@@ -346,8 +346,7 @@ SEIRModel.generateData = function(region, interventions, days) {
 
   let data = {}
   COMPARTMENTS.forEach((c,i) => data[c] = aggregates[i])
-  let cohortDeaths = np.unpack(results.pick(null, null, 5))
-  data["cohortDeaths"] = zip(...cohortDeaths).map(a => a.reduce((s,n) => s+n))
+  data["cohortDeaths"] = np.unpack(results.pick(END_DAY, null, 5))
 
   return data
 }
